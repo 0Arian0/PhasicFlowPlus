963f5f01ab1bd6e55e96ebeadd8ea2b6838f5dda
Hamidreza <hamid.r.norouzi@gmail.com>
2026-09-13 23:00:03 +0330

    Correction for the momentumExchange when cell or distribution is used

--- phasicFlowCoupling/couplingSystem/unresolved/interaction/momentumInteraction/momentumInteraction/momentumInteraction.C
+++ phasicFlowCoupling/couplingSystem/unresolved/interaction/momentumInteraction/momentumInteraction/momentumInteraction.C
@@ -78,7 +78,7 @@ momentumInteraction::momentumInteraction
         INFORMATION<<"Cell distribution is active."<<END_INFO;
     }
     
-    if(momentumExchangeDistribute_)
+    if(!momentumExchangeDistribute_)
     {
         noDistribution_ = makeUnique<PCM>(uCS.cMesh(), uCS.centerMass());
     }
@@ -121,7 +121,7 @@ void momentumInteraction::calculateCoupling
         *fluidAveraging_,
         *solidAveraging_,
         porosity_.particleDiameter(),
-        momentumExchangeDistribute_ ? noDistribution_() : uCS().distribution(),
+        momentumExchangeDistribute_ ? uCS().distribution():noDistribution_(),
         fluidForce);
 
     lift_->calculateLiftForceTorque(
